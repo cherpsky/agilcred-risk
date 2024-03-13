b612d9ddc7a884bfc3bcf818a61767f25273bbed
Andrés Bandera <abandera@grainchain.io>
2024-03-13 09:54:32 -0500

    Se quitaron unos logs de config de la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
-        console.log('CONFIG', config.get('DATABASE_HOST'));
-        console.log('CONFIG', config.get('DATABASE_PORT'));
-        console.log('CONFIG', config.get('DATABASE_USER'));
-        console.log('CONFIG', config.get('DATABASE_PASSWORD'));
-        console.log('CONFIG', config.get('DATABASE_NAME'));
         return {
           type: 'mysql',
           host: config.get('DATABASE_HOST'),
